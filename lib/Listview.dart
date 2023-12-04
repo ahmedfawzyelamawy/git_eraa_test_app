d91673a34de92e273db10943a61a32ac139c8972
ahmedfawzyelamawy <ahmedelamawy78@gmail.com>
2023-12-04 13:58:17 +0200

    amendment

--- lib/Listview.dart
+++ lib/Listview.dart
@@ -6,68 +6,88 @@ class Listview extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.grey,
-        ),
-        body:
+      appBar: AppBar(
+        backgroundColor: Colors.grey,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 120,
+              child: ListView.separated(
+                  scrollDirection: Axis.horizontal,
+                  padding: EdgeInsetsDirectional.all(15),
+                  itemBuilder: (context, index) {
+                    return Row(
+                      children: [
+                        Column(
+                          children: [
+                            CircleAvatar(
+                              backgroundImage:
+                                  AssetImage('assets/images/profile.jpg'),
+                              radius: 35,
+                            ),
+                            Text(
+                              'ahmed',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            )
+                          ],
+                        )
+                      ],
+                    );
+                  },
+                  separatorBuilder: (context, index) {
+                    return SizedBox(
+                      width: 7,
+                    );
+                  },
+                  itemCount: 10),
+            ),
             ListView.separated(
-                scrollDirection: Axis.horizontal,
-                padding: EdgeInsetsDirectional.all(15),
-                itemBuilder:(context, index) {
-                  return Row(
+                physics: NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                padding: EdgeInsetsDirectional.all(12),
+                itemBuilder: (context, index) {
+                  return Column(
                     children: [
-                      Column(
+                      Row(
                         children: [
-                          CircleAvatar(backgroundImage: AssetImage('assets/images/profile.jpg'),radius: 35,),
-                          Text('ahmed',style: TextStyle(fontWeight: FontWeight.bold),)
+                          CircleAvatar(
+                              backgroundImage:
+                                  AssetImage('assets/images/profile.jpg'),
+                              radius: 40),
+                          Expanded(
+                            child: Column(
+                              children: [
+                                Text(
+                                  'Ahmed fawzy',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 18),
+                                ),
+                                Text('Wlcome to flutter',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 18)),
+                              ],
+                            ),
+                          ),
+                          Text('11:37 pm')
                         ],
                       )
                     ],
                   );
                 },
-                separatorBuilder:  (context, index) {
-                  return SizedBox(width: 7,);
+                separatorBuilder: (context, index) {
+                  return Divider(
+                    color: Colors.black,
+                    thickness: 1,
+                  );
                 },
-                itemCount: 10 ),
-            // ListView.separated(
-            //     padding: EdgeInsetsDirectional.all(12),
-            //     itemBuilder: (context, index) {
-            //       return Column(
-            //         children: [
-            //           Row(
-            //             children: [
-            //               CircleAvatar(
-            //                   backgroundImage:
-            //                   AssetImage('assets/images/profile.jpg'),
-            //                   radius: 40),
-            //               Expanded(
-            //                 child: Column(
-            //                   children: [
-            //                     Text(
-            //                       'Ahmed fawzy',
-            //                       style: TextStyle(
-            //                           fontWeight: FontWeight.bold, fontSize: 18),
-            //                     ),
-            //                     Text('Wlcome to flutter',
-            //                         style: TextStyle(
-            //                             fontWeight: FontWeight.bold, fontSize: 18)),
-            //                   ],
-            //                 ),
-            //               ),
-            //               Text('11:37 pm')
-            //             ],
-            //           )
-            //         ],
-            //       );
-            //     },
-            //     separatorBuilder: (context, index) {
-            //       return Divider(
-            //         color: Colors.black,
-            //         thickness: 1,
-            //       );
-            //     },
-            //     itemCount: 12),
-
+                itemCount: 12),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:  Session(),
+      home:  Listview(),
     ),
   );
 }
